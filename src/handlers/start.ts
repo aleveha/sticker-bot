a4005874f9e42bd9373e020d648674f84a75b66d
Nikita Tashchilin <nikita.tashilin@gmail.com>
2024-08-21 15:39:43 -0400

    feat: add 'add to chat' button to start command message

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -1,9 +1,22 @@
 import type { CommandContext } from "grammy";
+import type { InlineKeyboardMarkup } from "grammy/types";
 import type { BotContext } from "~/types.ts";
 
 export async function startHandler(ctx: CommandContext<BotContext>) {
-	await ctx.reply(ctx.t("start"), {
-		reply_parameters: ctx.message?.message_id ? { message_id: ctx.message.message_id } : undefined,
+	const replyMarkup: InlineKeyboardMarkup = {
+		inline_keyboard: [
+			[
+				{
+					text: ctx.t("start-command.button"),
+					url: `https://t.me/${ctx.me.username}?startgroup=${ctx.me.username}`,
+				},
+			],
+		],
+	};
+
+	await ctx.reply(ctx.t("start-command.message"), {
+		reply_parameters: { message_id: ctx.msg?.message_id, allow_sending_without_reply: true },
 		parse_mode: "HTML",
+		reply_markup: ctx.chat.type === "private" ? replyMarkup : undefined,
 	});
 }
--- src/locales/en.ftl
+++ src/locales/en.ftl
@@ -1,6 +1,9 @@
-start =
-    Hey there! 👋🏻
+start-command =
+    .message =
+        Hey there! 👋🏻
 
-    I have a <b>huuuge</b> amount of stickers that I can send <b>automatically or on command</b>.
+        I have a <b>huuuge</b> amount of stickers that I can send <b>automatically or on command</b>.
 
-    <b>Hurry up and add me to your chat and check out how many stickerpacks you haven't seen yet!</b> 🔥
+        <b>Hurry up and add me to your chat and check out how many stickerpacks you haven't seen yet!</b> 🔥
+
+    .button = Try me in your chat
